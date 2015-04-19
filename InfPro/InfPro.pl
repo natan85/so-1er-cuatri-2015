ecf84a1805dd9e87c73c7e3c67e82de39667c837
romicorty <romicorty@gmail.com>
2015-04-19 17:37:46 -0300

    Se agrega menú y validación de parametros.

--- InfPro/InfPro.pl
+++ InfPro/InfPro.pl
@@ -1,8 +1,11 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 use strict;
 use warnings;
 use Fcntl qw(:flock);
+use feature qw/switch/; 
 
+
+###### Se valida que el comando no este en ejecución#####
 open(my $script_fh, '<', $0)
    or die("No se pudo abrir el archivo: $!\n");
 
@@ -11,6 +14,58 @@ unless (flock($script_fh, LOCK_EX|LOCK_NB)) {
     exit(1);
 }
 
-print "ejecutando...\n";
-sleep(15);
-print "Fin del programa\n";
+###### MENU ##############################
+###### Si no ingresa ningún parametro se mostrará la ayuda
+my $num_parametros = $#ARGV + 1;
+
+if ($num_parametros < 1) {
+    print "Debe ingresar una opción.\n";
+    mostrarAyuda();
+    exit;
+}else{
+	my $primero=$ARGV[0];
+
+	if($primero eq "-a") { mostrarAyuda(); }
+	elsif ($primero eq "-c") { consultar();}
+	elsif($primero eq "-g") { grabar();}
+	elsif($primero eq "-i") { informar(); }
+	elsif($primero eq "-e") { estadisticas();}
+	else { 
+			print "Opción incorrecta.\n";
+    		mostrarAyuda();
+    		exit;
+    	}
+}
+
+####### Declaración de funciones ###### 
+ 
+sub mostrarAyuda {
+	print "Ayuda: InfPro [opción] [argumentos] 
+		  -a                  Muestra esta ayuda
+		  -g                  Grabar
+		  -c[palabra clave]   Consulta por palabra clave
+		  -i                  Información
+		  -e                  Estadísticas\n";
+}
+
+sub grabar{
+	print "Se eligió -g\n"; 
+}
+
+sub consultar{
+	my $num_par = $#ARGV + 1;
+	if($num_parametros == 1){
+		print "Debe ingresar al menos una palabra clave\n";
+	}else{
+		print "Se eligió -c\n";	
+	}
+	 
+}
+
+sub informar{
+	print "Se eligió -i\n";
+}
+
+sub estadisticas{
+	print "Se eligió -e\n"; 
+}
