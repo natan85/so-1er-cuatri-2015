9de50af1db94b21c69dd2624f3e9f2582deafcfe
mrojas2005 <mrojas2005@users.noreply.github.com>
2015-04-24 05:58:36 -0300

    Update Glog.sh

--- Glog/Glog.sh
+++ Glog/Glog.sh
@@ -1,62 +1,65 @@
-# ************************************************ #
+# *******************************************************************
+# *******************************************************************
 # Sistemas Operativos - 1er cuat 2015
 # Grupo 8 - Trabajo Practico H
-# ************************************************ #
-# Uso: Glog.sh <comando> <mensaje> [codigo]
-# ************************************************ #
-# comando: nombre del comando que escribe en su log
-# mensaje: texto del mensaje a escribir en el log
-# codigo opcional: 
+# *******************************************************************
+# Uso: ./Glog.sh <comando> <mensaje> [codigo]
+# *******************************************************************
+# Los tres parametros son cadenas de caracteres entre comillas dobles.
+# <comando> (obligatorio): nombre del comando que escribe en su log
+# <mensaje> (obligatotio): texto del mensaje a escribir en el log
+# [codigo] (opcional): alguno de los siguientes codigos:
 # INFO (Informativo)
-# WAR (Warning)
+# WAR (Warning/Aviso)
 # ERR (Error)
-# ************************************************ #
+# *******************************************************************
+# *******************************************************************
+# Ejemplos de uso:
+# ./Glog.sh "RecPro" "Nuevos archivos recibidos" "INFO"
+# ./Glog.sh "IniPro" "El sistema se inicializo parcialmente" "WAR"
+# ./Glog.sh "ProPro" "Error en codigo de protocolizacion" "ERR"
+# *******************************************************************
+# *******************************************************************
 
 #!/bin/bash
 
-# ***************************************************
+# *******************************************************************
 # Definiciones
-# ***************************************************
+# *******************************************************************
 
-# usuario del sistema
+# usuario del sistema en FIUBA
 USUARIO="alumnos"
-
-# ubicacion esperada del archivo de configuracion
-CONF=../CONFDIR/InsPro.conf
 
 # cantidad de lineas a conservar al recortar logs
 LINEAS=10
 
-# codigo por default (usado cuando no hay codigo)
+# codigo por default (cuando no se recibe codigo)
 CODIGO="INFO"
 
-# ***************************************************
+# ubicacion relativa esperada del archivo de configuracion
+CONFIG="../CONFDIR/InsPro.conf"
+
+# *******************************************************************
 # Verificaciones
-# ***************************************************
+# *******************************************************************
 
 # verificar existencia archivo configuracion
-if ! [ -e $CONF ]; 
+if ! [ -e $CONFIG ]; 
 then
-	echo "[Glog] El archivo de configuracion no existe"
+	echo "[Glog] Error: el archivo de configuracion no existe"
 	exit 1
 fi
 
-# directorio base del usuario
-#BASE=$(grep $USUARIO /etc/passwd | cut -f6 -d":")
-
 # obtener valores de configuracion
-GRUPO=`grep '^GRUPO=' $CONF | sed 's/^GRUPO=\([^=]*\).*/\1/'`
-LOGDIR=`grep '^LOGDIR=' $CONF | sed 's/^LOGDIR=\([^=]*\).*/\1/'`
-LOGSIZE=`grep '^LOGSIZE=' $CONF | sed 's/^LOGSIZE=\([^=]*\).*/\1/'`
+GRUPO=`grep '^GRUPO=' $CONFIG | sed 's/^GRUPO=\([^=]*\).*/\1/'`
+LOGDIR=`grep '^LOGDIR=' $CONFIG | sed 's/^LOGDIR=\([^=]*\).*/\1/'`
+LOGSIZE=`grep '^LOGSIZE=' $CONFIG | sed 's/^LOGSIZE=\([^=]*\).*/\1/'`
 
-# directorio de archivos de log
-DIR=$HOME/$GRUPO/$LOGDIR/
-
-# verificar existencia directorio de logs, o crearlo
-if ! [ -d $DIR ]; 
+# verificar valores de configuracion obtenidos
+if [ -z $GRUPO ] || [ -z $LOGDIR ] || [ -z $LOGSIZE ];
 then
-	echo "[Glog] El directorio para logs no existe, se creara"
-	mkdir -p $DIR
+	echo "[Glog] Error en valores de configuracion"
+	exit 1
 fi
 
 # verificar cantidad de parametros recibidos
@@ -66,7 +69,7 @@ then
 	exit 1
 fi
 
-# verificar el codigo opcional
+# verificar si hay codigo recibido
 if [ $# -ge 3 ];
 then
 	if [ $3 != "INFO" ] && [ $3 != "WAR" ] && [ $3 != "ERR" ];
@@ -78,6 +81,16 @@ then
 	fi
 fi
 
+# directorio del archivo de log
+DIR=$GRUPO/$LOGDIR/$1
+
+# verificar existencia directorio de log, o crearlo
+if ! [ -d $DIR ]; 
+then
+	mkdir $DIR
+	echo "[Glog] Fue creado el directorio $DIR"
+fi
+
 # ***************************************************
 # Escribir en el log
 # ***************************************************
@@ -86,34 +99,44 @@ fi
 fecha=`date +"%d-%m-%Y %H:%M:%S"`
 usuario=$USUARIO
 comando=$1
-mensaje=$2
 codigo=$CODIGO
+mensaje=$2
 
-# lugar donde escribir
+# donde escribir
 nombre="$1.log"
-archivo=$DIR$nombre
+archivo=$DIR/$nombre
 
 # escribir en el log
-echo "$fecha-$usuario-$comando-$mensaje-$codigo" >> "$archivo"
+echo "$fecha-$usuario-$comando-$codigo-$mensaje" >> "$archivo"
 
 # ***************************************************
 # Recortar archivo de log si es necesario
 # ***************************************************
 
 # obtener tamanio archivo
-tamanio=$(stat -c%s "$archivo")
+tamanio=$(stat -c %s "$archivo")
 
 # recortar archivo si excedio el tamanio maximo
 if [ $tamanio -gt $LOGSIZE ];
 then
+	# copiar las utimas lineas en un archivo temporal
 	cat "$archivo" | tail -n $LINEAS > "$archivo.tmp";
-	mv -f "$archivo.tmp" "$archivo"	
-	fecha=`date +"%d-%m-%Y %H:%M:%S"`;
+	
+	# mover el archivo temporal sobre el original
+	mv -f "$archivo.tmp" "$archivo"
+	
+	# datos para escribir en el log
+	fecha=`date +"%d-%m-%Y %H:%M:%S"`
+	usuario=$USUARIO
 	comando="Glog"	
-	mensaje="Archivo log excedido, se recorto"
-	codigo="INFO"	    	
-	echo "$fecha-$usuario-$comando-$mensaje-$codigo" >> "$archivo"
-	echo "$fecha -> Archivo $nombre excedido, se recorto"
+	codigo="WAR"
+	mensaje="Archivo log excedido, se recorto"	
+	
+	# escribir en archivo de log    	
+	echo "$fecha-$usuario-$comando-$codigo-$mensaje" >> "$archivo"
+	
+	# mostrar por pantalla	 
+	echo "[Glog] Archivo $nombre excedido, se recorto"
 fi
 
 exit 0
