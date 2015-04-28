0c6702b6ae68226fe01c4277e56c52a78a59738a
Romicorty <romicorty@gmail.com>
2015-04-28 20:57:31 -0300

    Se corrige la asignación de puntajes y se da formato a la información a mostrar y grabar

--- InfPro/InfPro.pl
+++ InfPro/InfPro.pl
@@ -3,8 +3,9 @@ use strict;
 use warnings;
 use Fcntl qw(:flock);
 use feature qw/switch/; 
-
-
+our %filtros = ('-ft' => " ", '-fa' => " ", '-fn' => " ", '-fg' => " ", '-fe' => " ");
+our @a_resultados;
+our %h_contenido_resultados;
 ###### Se valida que el comando no este en ejecución#####
 open(my $script_fh, '<', $0)
    or die("No se pudo abrir el archivo: $!\n");
@@ -60,7 +61,13 @@ sub grabar{
 	print "Se eligió -g\n"; 
 	my $ruta = $ENV{"INFODIR"};
 	my $epoc = time();
-	my $str = "Dato de prueba.";
+	#ARMO CONTENIDO
+	my $s1 = 'Search'."\n";
+	my $s2 = 'Tools'."\n";
+	#my $s3 = "$s1$s2";
+	my $str = "$s1$s2\n";
+	# $s3 contains 'PerlMonks'
+	#my $str = "Dato de prueba.";
 	my $nombreArchivo = $ruta."/resultado_".$epoc;
 	open FILE, ">".$nombreArchivo or die $!; 
 	print FILE $str; 
@@ -69,17 +76,13 @@ sub grabar{
 
 sub consultar{
 	my $num_parametros = $#ARGV + 1;
-	if($num_parametros == 1){
-		print "Debe ingresar al menos una palabra clave\n";
+	
+	if($num_parametros < 4 ){
+		print "Debe utilizar al menos un filtro\n";	
 	}else{
-		if($num_parametros < 4 ){
-			print "Debe utilizar al menos un filtro\n";	
-		}else{
-			print "Se eligió -c\n";
-			leerTodosArchivos();
-		}
+		print "Se eligió -c\n";
+		leerTodosArchivos();
 	}
-	 
 }
 
 #Para ordenar los archivos primero debo pasar el hash a una lista
@@ -97,10 +100,10 @@ sub devolverArchivosOrdenados{
 
 sub leerTodosArchivos{
 	#my $palabraClave = shift;
-	my %filtros = ('-ft' => " ", '-fa' => " ", '-fn' => " ", '-fg' => " ", '-fe' => " ");
+	#my %filtros = ('-ft' => " ", '-fa' => " ", '-fn' => " ", '-fg' => " ", '-fe' => " ");
 	my $pidioGuardar = 0;
-	my $palabraClave = $ARGV[1];	
-	my $ruta = $ENV{"NOVEDIR"};
+	my $palabraClave = $ARGV[1];
+	my $ruta = $ENV{"PROCDIR"};
 	opendir (DIR, $ruta) or die $!;
 	my %hashResultados;
 	#Cargamos los filtros
@@ -138,16 +141,16 @@ sub leerTodosArchivos{
 			#Inicializo variables de filtrado
 			my $cumplioCausal = 0;
 			my $cumplioExtracto = 0;
-			my $cumplioFiltroTipoNorma = 1;
-			my $cumplioFiltroAnio = 1;
-			my $cumplioFiltroNumeroNorma = 1;
-			my $cumplioFiltroGestion = 1;
-			my $cumplioFiltroEmisor = 1;
+			my $cumplioFiltroTipoNorma = 0;
+			my $cumplioFiltroAnio = 0;
+			my $cumplioFiltroNumeroNorma = 0;
+			my $cumplioFiltroGestion = 0;
+			my $cumplioFiltroEmisor = 0;
 
 			#Busco los valores de causal y extracto		
 			my @data = split(";",$row);
-			my $causal = $data[2];
-			my $extracto = $data[3];
+			my $causal = $data[4];
+			my $extracto = $data[5];
 			#leerArchivo($file);
 			if (defined $causal and index($causal, $palabraClave) != -1) {
 			   $cumplioCausal = 1; 
@@ -155,25 +158,70 @@ sub leerTodosArchivos{
 			if(defined $extracto and index($extracto, $palabraClave) != -1){
 			   $cumplioExtracto = 1;	
 			}
+			#Busco por filtro Tipo Norma
+			my $tipoNorma = $data[12];
+			my $normaIngresada = $filtros{'-ft'};
+			#printf "%-8s %s\n", $tipoNorma, $normaIngresada;
 			
+			if ($normaIngresada eq " "){
+			   $cumplioFiltroTipoNorma = 1;	
+			}
+			if (defined $tipoNorma and ($normaIngresada ne " ") and index($tipoNorma, $normaIngresada) != -1) {
+			   $cumplioFiltroTipoNorma = 1; 
+			}
+
+			#Busco por filtro Año
+			my $anio = $data[3];
+			my $anioIngresado = $filtros{'-fa'};
+			
+			if ($anioIngresado eq " "){
+			   $cumplioFiltroAnio = 1;	
+			}
+			if (defined $anio and ($anioIngresado ne " ") and index($anio, $anioIngresado) != -1) {
+			   $cumplioFiltroAnio = 1; 
+			}
+			#Busco por filtro numero Norma
+			my $numero = $data[2];
+			my $numeroIngresado = $filtros{'-fn'};
+				
+			if ($numeroIngresado eq " "){
+			   $cumplioFiltroNumeroNorma = 1;	
+			}
+			if (defined $numero and ($numeroIngresado ne " ") and index($numero, $numeroIngresado) != -1) {
+			   $cumplioFiltroNumeroNorma = 1; 
+			}
+			#Busco por filtro Gestion
+			my $gestion = $data[11];
+			my $gestionIngresada = $filtros{'-fg'};
+			
+			if ($gestionIngresada eq " "){
+			   $cumplioFiltroGestion = 1;	
+			}
+			
+			if (defined $gestion and index($gestion, $gestionIngresada) != -1) {
+			   $cumplioFiltroGestion = 1; 
+			}
+			#Busco por filtro Emisor
+			my $emisor = $data[13];
+			my $emisorIngresado = $filtros{'-fe'};
+			
+			if ($emisorIngresado eq " "){
+			   $cumplioFiltroEmisor = 1;	
+			}
+			
+			if (defined $emisor and index($emisor, $emisorIngresado) != -1) {
+			   $cumplioFiltroEmisor = 1; 
+			}
+
 			if (($cumplioCausal == 1 or $cumplioExtracto == 1) and $cumplioFiltroTipoNorma == 1 and $cumplioFiltroAnio == 1 and $cumplioFiltroNumeroNorma == 1 and $cumplioFiltroGestion == 1 and $cumplioFiltroEmisor == 1){
-				#Agrego al hash el puntaje del archivo - pasar a metodo
-				if($cumplioCausal == 1){
-					if(!exists($hashResultados{$file} ) ){
-						$hashResultados{$file} = 10;					
-					}else{
-						$hashResultados{$file} += 10;
-					}
-				}
-				if ($cumplioExtracto == 1) {
-				    	#Agrego al hash el puntaje del archivo - pasar a metodo
-					if( !exists($hashResultados{$file} ) ){
-						$hashResultados{$file} = 1;
-					}
-					else{
-						$hashResultados{$file} += 1;
-					}		
-				}	
+				#Armamos la salida
+				my $fechaNorma = $data[1];		
+				my $peso = obtenerPeso($causal, $extracto, $palabraClave);
+				my $renglon1 = $emisor." ".$numero."/".$anio." ".$gestion." ".$fechaNorma." ".$peso."\n";
+				my $renglon2 = $extracto."\n";
+				my $renglon3 = $causal."\n";
+				print "$renglon1$renglon2$renglon3\n"
+				
 			}
 	
 		}#Del while lineas
@@ -184,6 +232,16 @@ sub leerTodosArchivos{
 	    	printf "%-8s %s\n", $name, $hashResultados{$name};
 	}
 	
+}
+
+sub obtenerPeso(){
+	my $causal = $_[0];
+	my $extracto = $_[1];
+	my $palabraClave = $_[2];
+	my $contadorCausal = () = $causal =~ /$palabraClave/g;
+	my $contadorExtracto = () = $extracto =~ /$palabraClave/g;
+	my $peso = $contadorCausal*10+$contadorExtracto;
+	return $peso;
 }
 
 sub informar{
