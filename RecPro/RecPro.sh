f84be21e0c1fc5e46090f2b55fa1546dde8b7f07
bliberini <bliberini@gmail.com>
2015-05-02 18:35:51 -0300

    RecPro: avance hasta proceso de archivos pendientes

--- RecPro/RecPro.sh
+++ RecPro/RecPro.sh
@@ -7,6 +7,9 @@ grupo="home/bliberini/TP/so-1er-cuatri-2015"
 # Variable que guarda el path a NOVEDIR (reemplazar con la que se obtiene de configuración)
 novedir="$grupo/noveDirMock"
 
+# Variable que guarda el path a ACEPDIR (reemplazar con la que se obtiene de configuración)
+acepdir="$grupo/acepDirMock"
+
 # Variable que guarda el path a Glog (reemplazar con ./Glog.sh cuando se pongan todos en /bin)
 glog="$grupo/Glog/Glog.sh"
 
@@ -16,19 +19,106 @@ sleepTime=30
 # Variable que mantiene contador de ciclos de ejecución
 timesExe=0
 
+# Variable que guarda la dirección del directorio de maestros (reemplazar con la que se obtiene de configuración)
+maedir="$grupo/maeDirMock"
+
+# Variable que guarda la dirección del directorio de rechazados (reemplazar con la que se obtiene de configuración)
+rechdir="$grupo/rechDirMock"
+
+# Variable que guarda el registro de la gestión del archivo, si la encuentra
+gestion=""
+
+function moverRechazado {
+	filename=$1
+	pathOrigen="$novedir/$filename"
+	pathDestino="$rechdir/$filename"
+
+	# Falta invocar a MOVE
+}
+
+function moverArchivoValido {
+	pathOrigen="$novedir/$1"
+	pathDestino="$2/$1"
+
+	# Falta invocar a MOVE	
+}
+
+
 function verificarGestion {
+	gestion=$(grep -E "^$1" "$maedir/gestiones.mae")
+	if [ ! -n "$gestion" ]
+	then
+		$glog $cmdName "$2 rechazado: Gestión inválida" "ERROR"
+		moverRechazado $filename
+		continue
+	fi
 }
 
 function verificarNorma {
+	row=$(grep -E "^$1" "$maedir/normas.mae")
+	if [ ! -n "$row" ]
+	then
+		$glog $cmdName "$2 rechazado: Norma inválida" "ERROR"
+		moverRechazado $filename
+		continue
+	fi
 }
 
 function verificarEmisor {
+	row=$(grep -E "^$1" "$maedir/emisores.mae")
+	if [ ! -n "$row" ]
+	then
+		$glog $cmdName "$2 rechazado: Emisor inválido" "ERROR"
+		moverRechazado $filename
+		continue
+	fi
 }
 
-function verificarNroArchivo {
+function verificarNroArchivo {	
+	re='^[0-9]+$'
+	if ! [[ $1 =~ $re ]]
+	then
+		$glog $cmdName "$2 rechazado: Número de archivo inválido" "ERROR"
+		moverRechazado $filename		
+		continue
+	fi
 }
 
 function verificarFecha {
+	fecha=$1
+	if ! date -d "${fecha:6:4}-${fecha:3:2}-${fecha:0:2}" &> /dev/null
+	then
+		$glog $cmdName "$2 rechazado: Formato de fecha inválida" "ERROR"
+		moverRechazado $filename
+		continue
+	else
+		fechaIni=$(echo "$gestion" | cut -d';' -f 2)
+		fechaFin=$(echo "$gestion" | cut -d';' -f 3)
+		fechaIniDate=$(date -d "${fechaIni:6:4}-${fechaIni:3:2}-${fechaIni:0:2}" +"%Y%m%d")
+		if [ "$fechaFin" == "NULL" ]; then
+			fechaFinDate=$(date +"%Y%m%d")
+		else
+			fechaFinDate=$(date -d "${fecha:6:4}-${fecha:3:2}-${fecha:0:2}" +"%Y%m%d")
+		fi	
+		fechaReg=$(date -d "${fechaIni:6:4}-${fechaIni:3:2}-${fechaIni:0:2}" +"%Y%m%d")
+		if ! ([ $fechaIniDate -le $fechaReg ] && [ $fechaFinDate -ge $fechaReg ])
+		then
+			$glog $cmdName "$2 rechazado: Fecha fuera del rango de gestión" "ERROR"
+			moverRechazado $filename
+			continue	
+		fi		
+	fi
+}
+
+function verificarArchivoNoVacio {
+	filename=$1
+	contenido=$(cat "$novedir/$filename")
+	if [ ! -n "$contenido" ]
+	then
+		$glog $cmdName "$filename rechazado: Archivo vacío" "ERROR"
+		moverRechazado $filename
+		continue
+	fi
 }
 
 function verificarCamposArchivosValidos {
@@ -39,11 +129,12 @@ function verificarCamposArchivosValidos {
 	nroarchivo=${array[3]}
 	fechaarchivo=${array[4]}
 	
-	verificarGestion $codgestion
-	verificarNorma $codnorma
-	verificarEmisor $codemisor
-	verificarNroArchivo
-	verificarFecha
+	verificarGestion $codgestion $2
+	verificarNorma $codnorma $2
+	verificarEmisor $codemisor $2
+	verificarNroArchivo $nroarchivo $2
+	verificarFecha $fechaarchivo $2
+	verificarArchivoNoVacio $2
 }
 
 function verificarFormatoNombreArchivo {
@@ -51,10 +142,19 @@ function verificarFormatoNombreArchivo {
 	campos=$(echo "$filename" | tr "_" "\n")
 	if [ ${#campos[@]} -ne 5 ]
 	then
-		echo "$1 rechazado"
+		$glog $cmdName "$filename rechazado: Nombre de archivo inválido" "ERROR"
+		moverRechazado $filename
 	else
-		verificarCamposArchivosValidos $campos
+		verificarCamposArchivosValidos $campos $filename
 	fi	
+}
+
+function procesarArchivoValido {
+	filename=$1
+	gestionnom=$(echo $gestion | cut -d';' -f 1)
+	mkdir -p $acepdir/$gestionnom
+	moverArchivoValido $filename "$acepdir/$gestionnom"
+	$glog $cmdName "$filename aceptado: se mueve a $acepdir/$gestionnom/$filename" "INFO"
 }
 
 function procesarArchivosNuevos {
@@ -64,14 +164,16 @@ function procesarArchivosNuevos {
 		if [ $isText == "UTF-8" ] || [ $isText == "ASCII" ]
 		then
 			verificarFormatoNombreArchivo $archivo
+			procesarArchivoValido $filename
 		else
-			echo "$archivo rechazado"
+			$glog $cmdName "$archivo rechazado: No es un archivo de texto" "ERROR"
+			moverRechazado $filename
 		fi
 	done
 }
 
 function procesarArchivosPendientes {
-
+	pendientes=$()
 }
 
 function checkearArchivosNuevos {
@@ -88,5 +190,7 @@ while true
 do
 	timesExe=$(($timesExe + 1))
 	$glog $cmdName "RecPro ciclo nro. $timesExe" "INFO"
-	checkearArchivosNuevos	 
+	checkearArchivosNuevos
+	sleep $sleepTime	 
 done
+exit 0
